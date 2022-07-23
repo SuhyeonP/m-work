f7e3efecd6a358c1fd19eea0712de08c7ae3f4e8
suhyeonP <ahah12k@gmail.com>
2022-07-23 16:26:58 +0900

    :art: add layout ui

--- src/component/atom/Button.tsx
+++ src/component/atom/Button.tsx
@@ -1,17 +1,26 @@
 import { colors } from 'style/colors';
 import styled from '@emotion/styled';
+import { SerializedStyles } from '@emotion/react';
 
 interface IStyledProps {
   border_radius: string;
   color: keyof typeof colors;
   background_color: keyof typeof colors;
+  width: string;
+  height: string;
+  styles?: SerializedStyles;
 }
 
 export const ButtonStyled = styled.button<IStyledProps>(
-  ({ border_radius, color, background_color }) => `
+  ({ border_radius, color, background_color, width, height }) => `
   border-radius: ${border_radius};
-  color: ${color};
-  background-color: ${background_color};
+  color: ${colors[color]};
+  background-color: ${colors[background_color]};
+  width: ${width};
+  height: ${height};
+  
+  outline: 0;
+  border: 0;
 `
 );
 
@@ -24,11 +33,22 @@ export const Button = ({
   id,
   content,
   border_radius = '4px',
-  color = 'default_gray',
-  background_color = 'default_white',
+  color = 'default_white',
+  background_color = 'default_black',
+  width = '80px',
+  height = '40px',
+  styles,
 }: IProps): JSX.Element => {
   return (
-    <ButtonStyled id={id} background_color={background_color} color={color} border_radius={border_radius}>
+    <ButtonStyled
+      id={id}
+      background_color={background_color}
+      color={color}
+      border_radius={border_radius}
+      width={width}
+      height={height}
+      css={styles}
+    >
       {content}
     </ButtonStyled>
   );
--- src/component/molecules/Summary.tsx
+++ src/component/molecules/Summary.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { Button, Typography } from 'component/atom';
 import { Information } from 'types';
 import { colors } from 'style/colors';
+import { css } from '@emotion/react';
 
 const SummaryStyled = styled.div`
   display: flex;
@@ -10,6 +11,34 @@ const SummaryStyled = styled.div`
   border: 1px solid ${colors.default_black};
 
   height: 100px;
+  padding: 10px;
+  margin: 10px;
+
+  .inner-content {
+    display: flex;
+    flex-direction: column;
+
+    width: 70%;
+    max-width: 70%;
+    & > div {
+      margin: 5px 0;
+    }
+  }
+
+  .name-aliases {
+    display: flex;
+  }
+
+  .delete-button {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .count-info {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 type IProps = Information;
@@ -17,18 +46,20 @@ type IProps = Information;
 export const Summary = ({ books, name, titles, tvSeries, aliases }: IProps): JSX.Element => {
   return (
     <SummaryStyled>
-      <div>
-        <div>
-          <Typography content={name} />
-          <Typography content={aliases[0]} />
+      <div className="inner-content">
+        <div className="name-aliases">
+          <Typography content={`name: ${name}`} text_overFlow="ellipsis" />
+          <Typography content={`aliases: ${aliases.join(',')}`} text_overFlow="ellipsis" />
         </div>
-        <Typography content={titles.join(',')} />
-        <div>
+        <Typography content={`title: ${titles.join(',')}`} text_overFlow="ellipsis" />
+        <div className="count-info">
           <Typography content={`books: ${books.length}`} />
           <Typography content={`tvSeries: ${tvSeries.length}`} />
         </div>
       </div>
-      <Button content="삭제" id={String(name)} />
+      <div className="delete-button">
+        <Button content="삭제" id={String(name)} border_radius="20px" />
+      </div>
     </SummaryStyled>
   );
 };
