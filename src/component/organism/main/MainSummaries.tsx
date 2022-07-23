0c0191889f7658777e60eb2a6d06d8c4cf635310
suhyeonP <ahah12k@gmail.com>
2022-07-23 12:09:55 +0900

    :construction: before add intersection

--- src/component/organism/main/MainSummaries.tsx
+++ src/component/organism/main/MainSummaries.tsx
@@ -1,5 +1,7 @@
 import { Summary } from 'component/molecules';
 import styled from '@emotion/styled';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
 const MainSumariesStyled = styled.div`
   display: flex;
@@ -9,6 +11,17 @@ const MainSumariesStyled = styled.div`
 `;
 
 export const MainSummaries = (): JSX.Element => {
+  const [page, setPage] = useState(1);
+  const { data } = useInfiniteQuery(['infos', page], () =>
+    fetch(`https://www.anapioficeandfire.com/api/characters?page=${page}&pageSize=10`).then(data => data.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
   return (
     <MainSumariesStyled>
       <Summary name="홍길동" aliases="test" title="title" books={3} tvSeries={2} id="12" />
